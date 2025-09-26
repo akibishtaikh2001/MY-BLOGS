5b1636f85d209c6390abf7c291ac0c23a0dfcd02
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-09-26 18:51:12 +0600

    Create Fake Data for the blogs and load data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+
 import './App.css'
+import Blogs from './Components/Blogs/Blogs'
 import Navbar from './Components/Navbar/Navbar'
 
 function App() {
  
 
+
   return (
     <>
      
       <Navbar></Navbar>
+      <Blogs></Blogs>
      
      
     </>
