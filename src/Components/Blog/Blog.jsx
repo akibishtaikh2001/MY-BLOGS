3b06e9e65c2e32ab49c8775262f6eb04b5ee2e72
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-09-27 23:32:28 +0600

    Add img blog

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -6,21 +6,21 @@ const Blog = ({ blog }) => {
 
 
     return (
-       <div className="card bg-base-100 w-96 shadow-sm">
-  <figure className="px-10 pt-10">
-    <img
-      src={blog.cover}
-      alt="Shoes"
-      className="rounded-xl" />
-  </figure>
-  <div className="card-body items-end text-center">
-    <h2 className="card-title" > {blog.title} </h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div className="card-actions">
-      <button className="btn btn-primary">mark as a reading</button>
-    </div>
-  </div>
-</div>
+        <div className="card bg-base-100 w-96 shadow-sm">
+            <figure className="px-10 pt-10">
+                <img
+                    src={blog.cover}
+                    alt="Shoes"
+                    className="rounded-xl" />
+            </figure>
+            <div className="card-body items-end text-center">
+                <h2 className="card-title" > {blog.title} </h2>
+                <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                <div className="card-actions">
+                    <button className="btn btn-primary">mark as a reading</button>
+                </div>
+            </div>
+        </div>
     );
 };
 
