7f9599a1d87ee7d358b92e83b42c3e81f895bd38
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-09-28 15:17:56 +0600

    Add Bookmark button

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaBookmark } from "react-icons/fa";
 
 const Blog = ({ blog }) => {
     //    const { blog } = props;
@@ -6,18 +7,29 @@ const Blog = ({ blog }) => {
 
 
     return (
-        <div className="card bg-base-100 w-96 shadow-sm">
+        <div className="card bg-base-100 w-96 shadow-sm m-2">
             <figure className="px-10 pt-10">
                 <img
                     src={blog.cover}
                     alt="Shoes"
                     className="rounded-xl" />
             </figure>
-            <div className="card-body items-end text-center">
+            <div className="card-body  text-center">
+                <div className="author flex justify-around items-center  ">
+                    <h3> {blog.author} </h3>
+                    <img className=' w-16 ' src={blog.author_img} alt="" />
+                    <FaBookmark size={25} />
+                </div>
                 <h2 className="card-title" > {blog.title}  </h2>
                 <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                <div className="card-actions">
-                    <button className="btn btn-primary">mark as a reading</button> 
+                <div className=' flex '>
+                    {
+                        blog.hashtags.map(hash => <p> {hash} </p>)
+                    }
+
+                </div>
+                    <div className="card-actions justify-end  ">
+                    <button className="btn btn-primary">mark as a reading</button>
                 </div>
             </div>
         </div>
